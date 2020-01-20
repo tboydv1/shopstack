928ef9f32bcab1c05b1ba6936f0f462ccf0c8f35
tboydv1 <tboydv1@gmail.com>
2020-01-20 15:05:27 +0100

    PROJECT SETUP

---
+++ shopstack_data/src/test/java/com/shopstack/db/DbConnectionTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.shopstack.db;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author oluwatobi
+ *
+ */
+public class DbConnectionTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
