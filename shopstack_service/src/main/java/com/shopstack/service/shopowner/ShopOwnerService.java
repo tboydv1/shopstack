f822060c055dc27b2c75df9c3cb81da551ba0cba
tboydv1 <tboydv1@gmail.com>
2020-01-25 17:55:45 +0100

    ShopOwner service implementation and test classes

---
+++ shopstack_service/src/main/java/com/shopstack/service/shopowner/ShopOwnerService.java
@@ -0,0 +1,9 @@
+package com.shopstack.service.shopowner;
+
+import com.shopstack.entities.shopowner.ShopOwner;
+
+public interface ShopOwnerService {
+
+	public void addShopOwner(ShopOwner shopOwner);
+		
+}
