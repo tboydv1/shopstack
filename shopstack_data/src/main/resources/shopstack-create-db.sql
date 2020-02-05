5797558984517690510ea66b72a3ea1ec786c04f
tboydv1 <tboydv1@gmail.com>
2020-02-05 14:55:41 +0100

    sql update

--- shopstack_data/src/main/resources/shopstack-create-db.sql
+++ shopstack_data/src/main/resources/shopstack-create-db.sql
@@ -37,12 +37,12 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `shopstack`.`verification_token`
+-- Table `mydb`.`verification_token`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `shopstack`.`verification_token` ;
 
 CREATE TABLE IF NOT EXISTS `shopstack`.`verification_token` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL,
   `token` VARCHAR(45) NOT NULL,
   `expiry_date` DATE NULL,
   `ss_user_id` INT(11) NOT NULL,
@@ -62,117 +62,11 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `shopstack`.`category` ;
 
 CREATE TABLE IF NOT EXISTS `shopstack`.`category` (
-  `category_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `category_id` INT(11) NOT NULL,
   `category_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`category_id`),
   UNIQUE INDEX `category_name_UNIQUE` (`category_name` ASC))
 ENGINE = InnoDB;
-
- USE `shopstack` ;
-
--- -----------------------------------------------------
--- Table `shopstack`.`customer`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `shopstack`.`customer` ;
-
-CREATE TABLE IF NOT EXISTS `shopstack`.`customer` (
-  `customer_id` INT(11) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `organization_name` VARCHAR(45) NULL DEFAULT NULL,
-  `contact_number` INT(11) NOT NULL,
-  PRIMARY KEY (`customer_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `shopstack`.`product`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `shopstack`.`product` ;
-
-CREATE TABLE IF NOT EXISTS `shopstack`.`product` (
-  `product_code` INT(11) NOT NULL,
-  `product_name` VARCHAR(45) NOT NULL,
-  `purchase_date` DATE NOT NULL,
-  `expiry_date` DATE NULL DEFAULT NULL,
-  `rate` DOUBLE NOT NULL,
-  `decription` VARCHAR(45) NULL DEFAULT NULL,
-  `category_id` INT(11) NOT NULL,
-  PRIMARY KEY (`product_code`),
-  INDEX `fk_product_category1_idx` (`category_id` ASC),
-  CONSTRAINT `fk_product_category1`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `shopstack`.`category` (`category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `shopstack`.`inventory`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `shopstack`.`inventory` ;
-
-CREATE TABLE IF NOT EXISTS `shopstack`.`inventory` (
-  `inventory_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `product_code` INT(11) NOT NULL,
-  `opening_stock` INT(11) NULL DEFAULT NULL,
-  `closing_stock` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`inventory_id`),
-  INDEX `fk_inventory_product1_idx` (`product_code` ASC),
-  CONSTRAINT `fk_inventory_product1`
-    FOREIGN KEY (`product_code`)
-    REFERENCES `shopstack`.`product` (`product_code`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `shopstack`.`supplier`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `shopstack`.`supplier` ;
-
-CREATE TABLE IF NOT EXISTS `shopstack`.`supplier` (
-  `supplier_id` INT(11) NOT NULL,
-  `first_name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `organization_name` VARCHAR(45) NULL DEFAULT NULL,
-  `contact_number` INT(11) NOT NULL,
-  PRIMARY KEY (`supplier_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `shopstack`.`transaction_item`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `shopstack`.`transaction_item` ;
-
-CREATE TABLE IF NOT EXISTS `shopstack`.`transaction_item` (
-  `product_code` INT(11) NOT NULL,
-  `transaction_id` INT(11) NOT NULL,
-  `quantity` INT(11) NOT NULL,
-  `description` VARCHAR(45) NULL DEFAULT NULL,
-  `type` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`product_code`, `transaction_id`),
-  INDEX `fk_transaction_item_transaction1_idx` (`transaction_id` ASC),
-  CONSTRAINT `fk_transaction_item_product1`
-    FOREIGN KEY (`product_code`)
-    REFERENCES `shopstack`.`product` (`product_code`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_transaction_item_transaction1`
-    FOREIGN KEY (`transaction_id`)
-    REFERENCES `shopstack`.`transaction` (`transaction_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
@@ -196,28 +90,58 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `shopstack`.`transaction`
+-- Table `shopstack`.`ss_supplier`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `shopstack`.`transaction` ;
+DROP TABLE IF EXISTS `shopstack`.`ss_supplier` ;
 
-CREATE TABLE IF NOT EXISTS `shopstack`.`transaction` (
-  `transaction_id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `shopstack`.`ss_supplier` (
+  `supplier_id` INT(11) NOT NULL,
+  `ss_display_name` VARCHAR(45) NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `company` VARCHAR(45) NULL DEFAULT NULL,
+  `phone` INT(11) NOT NULL,
+  `Title` VARCHAR(45) NULL,
+  `middle_name` VARCHAR(45) NULL,
+  `mobile` VARCHAR(45) NULL,
+  `account_no` VARCHAR(45) NULL,
+  PRIMARY KEY (`supplier_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `shopstack`.`ss_product`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopstack`.`ss_product` ;
+
+CREATE TABLE IF NOT EXISTS `shopstack`.`ss_product` (
+  `product_code` INT(11) NOT NULL,
+  `product_name` VARCHAR(45) NOT NULL,
+  `purchase_date` DATE NOT NULL,
+  `category_id` INT(11) NOT NULL,
+  `price` DOUBLE NOT NULL,
+  `decription` VARCHAR(45) NULL DEFAULT NULL,
   `shop_shop_id` INT(11) NOT NULL,
-  `transaction_item_id` INT(11) NOT NULL,
-  `status` VARCHAR(45) NULL DEFAULT NULL,
-  `total` DOUBLE NULL DEFAULT NULL,
-  `transaction_date` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`transaction_id`),
-  INDEX `fk_transaction_item_id_idx` (`transaction_item_id` ASC),
-  INDEX `fk_transaction_shop1_idx` (`shop_shop_id` ASC),
-  CONSTRAINT `fk_transaction_item_id`
-    FOREIGN KEY (`transaction_item_id`)
-    REFERENCES `shopstack`.`transaction_item` (`product_code`)
+  `ss_supplier_supplier_id` INT(11) NULL,
+  PRIMARY KEY (`product_code`),
+  INDEX `fk_product_category1_idx` (`category_id` ASC),
+  INDEX `fk_ss_product_shop1_idx` (`shop_shop_id` ASC),
+  INDEX `fk_ss_product_ss_supplier1_idx` (`ss_supplier_supplier_id` ASC),
+  CONSTRAINT `fk_product_category1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `shopstack`.`category` (`category_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_transaction_shop1`
+  CONSTRAINT `fk_ss_product_shop1`
     FOREIGN KEY (`shop_shop_id`)
     REFERENCES `shopstack`.`shop` (`shop_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ss_product_ss_supplier1`
+    FOREIGN KEY (`ss_supplier_supplier_id`)
+    REFERENCES `shopstack`.`ss_supplier` (`supplier_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -225,39 +149,157 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `shopstack`.`invoice`
+-- Table `shopstack`.`line_item`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `shopstack`.`invoice` ;
+DROP TABLE IF EXISTS `shopstack`.`line_item` ;
 
-CREATE TABLE IF NOT EXISTS `shopstack`.`invoice` (
+CREATE TABLE IF NOT EXISTS `shopstack`.`line_item` (
+  `ss_product_code` INT(11) NOT NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
+  `quantity` INT(11) NOT NULL,
+  `price` DOUBLE NOT NULL,
+  `amount` DOUBLE NOT NULL,
+  PRIMARY KEY (`ss_product_code`),
+  INDEX `fk_line_item_ss_product1_idx` (`ss_product_code` ASC),
+  CONSTRAINT `fk_line_item_ss_product1`
+    FOREIGN KEY (`ss_product_code`)
+    REFERENCES `shopstack`.`ss_product` (`product_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`cash_invoice`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopstack`.`cash_invoice` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`cash_invoice` (
   `invoice_id` INT(11) NOT NULL,
-  `transaction_id` INT(11) NOT NULL,
-  `customer_id` INT(11) NULL DEFAULT NULL,
-  `supplier_id` INT(11) NULL DEFAULT NULL,
+  `line_item_ss_product_code` INT(11) NOT NULL,
+  `invoice_date` DATE NOT NULL,
+  `sub_total` DOUBLE NULL,
+  `tax` DOUBLE NULL,
+  `total` DOUBLE NULL,
+  `description` VARCHAR(145) NULL,
+  PRIMARY KEY (`invoice_id`),
+  INDEX `fk_cash_invoice_line_item1_idx` (`line_item_ss_product_code` ASC),
+  CONSTRAINT `fk_cash_invoice_line_item1`
+    FOREIGN KEY (`line_item_ss_product_code`)
+    REFERENCES `shopstack`.`line_item` (`ss_product_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `shopstack` ;
+
+-- -----------------------------------------------------
+-- Table `shopstack`.`ss_customer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopstack`.`ss_customer` ;
+
+CREATE TABLE IF NOT EXISTS `shopstack`.`ss_customer` (
+  `customer_id` INT(11) NOT NULL,
+  `ss_display_name` VARCHAR(45) NOT NULL,
+  `ss_customer_title` VARCHAR(45) NULL,
+  `ss_customer_email` VARCHAR(45) NOT NULL,
+  `ss_customer_firstname` VARCHAR(45) NULL,
+  `company` VARCHAR(45) NULL DEFAULT NULL,
+  `ss_customer_phone` VARCHAR(45) NULL,
+  `ss_customer_lastname` VARCHAR(45) NULL,
+  `ss_customer_middlename` VARCHAR(45) NULL,
+  PRIMARY KEY (`customer_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `shopstack`.`inventory`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopstack`.`inventory` ;
+
+CREATE TABLE IF NOT EXISTS `shopstack`.`inventory` (
+  `inventory_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `quantity_on_hand` INT(11) NOT NULL,
+  `description` VARCHAR(145) NULL,
+  `product_code` INT(11) NOT NULL,
+  PRIMARY KEY (`inventory_id`, `product_code`),
+  INDEX `fk_inventory_product1_idx` (`product_code` ASC),
+  CONSTRAINT `fk_inventory_product1`
+    FOREIGN KEY (`product_code`)
+    REFERENCES `shopstack`.`ss_product` (`product_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `shopstack`.`credit_invoice`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopstack`.`credit_invoice` ;
+
+CREATE TABLE IF NOT EXISTS `shopstack`.`credit_invoice` (
+  `invoice_id` INT(11) NOT NULL,
+  `ss_customer_id` INT(11) NOT NULL,
+  `balance_due` DOUBLE NOT NULL,
+  `invoice_date` DATETIME NOT NULL,
+  `invoice_due_date` VARCHAR(45) NOT NULL,
+  `line_item_ss_product_code` INT(11) NOT NULL,
   `discount` DOUBLE NULL DEFAULT NULL,
   `tax` DOUBLE NULL DEFAULT NULL,
-  `payments` DOUBLE NOT NULL,
-  `balance` DOUBLE NOT NULL,
+  `payments` DOUBLE NULL,
+  `subtotal` VARCHAR(45) NULL,
   `total` DOUBLE NOT NULL,
-  `notes` VARCHAR(100) NULL DEFAULT NULL,
-  `created_on` DATETIME NOT NULL,
+  `invoice_notes` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`invoice_id`),
-  INDEX `fk_invoice_customer1_idx` (`customer_id` ASC),
-  INDEX `fk_transaction_item_1_idx` (`transaction_id` ASC),
-  INDEX `fk_invoice_vendor1_idx` (`supplier_id` ASC),
-  CONSTRAINT `fk_invoice_customer1`
-    FOREIGN KEY (`customer_id`)
-    REFERENCES `shopstack`.`customer` (`customer_id`)
+  INDEX `fk_invoice_ss_customer1_idx` (`ss_customer_id` ASC),
+  INDEX `fk_invoice_line_item1_idx` (`line_item_ss_product_code` ASC),
+  CONSTRAINT `fk_invoice_ss_customer1`
+    FOREIGN KEY (`ss_customer_id`)
+    REFERENCES `shopstack`.`ss_customer` (`customer_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoice_vendor1`
-    FOREIGN KEY (`supplier_id`)
-    REFERENCES `shopstack`.`supplier` (`supplier_id`)
+  CONSTRAINT `fk_invoice_line_item1`
+    FOREIGN KEY (`line_item_ss_product_code`)
+    REFERENCES `shopstack`.`line_item` (`ss_product_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `shopstack`.`transaction`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopstack`.`transaction` ;
+
+CREATE TABLE IF NOT EXISTS `shopstack`.`transaction` (
+  `transaction_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `status` VARCHAR(45) NULL DEFAULT NULL,
+  `started_date` DATETIME NOT NULL,
+  `date_completed` DATE NOT NULL,
+  `shop_shop_id` INT(11) NOT NULL,
+  `cash_invoice_id` INT(11) NOT NULL,
+  `credit_invoice_id` INT(11) NOT NULL,
+  PRIMARY KEY (`transaction_id`, `shop_shop_id`),
+  INDEX `fk_transaction_shop1_idx` (`shop_shop_id` ASC),
+  INDEX `fk_transaction_cash_invoice1_idx` (`cash_invoice_id` ASC),
+  INDEX `fk_transaction_credit_invoice1_idx` (`credit_invoice_id` ASC),
+  CONSTRAINT `fk_transaction_shop1`
+    FOREIGN KEY (`shop_shop_id`)
+    REFERENCES `shopstack`.`shop` (`shop_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_transaction_item_1`
-    FOREIGN KEY (`transaction_id`)
-    REFERENCES `shopstack`.`transaction` (`transaction_id`)
+  CONSTRAINT `fk_transaction_cash_invoice1`
+    FOREIGN KEY (`cash_invoice_id`)
+    REFERENCES `shopstack`.`cash_invoice` (`invoice_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_transaction_credit_invoice1`
+    FOREIGN KEY (`credit_invoice_id`)
+    REFERENCES `shopstack`.`credit_invoice` (`invoice_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
