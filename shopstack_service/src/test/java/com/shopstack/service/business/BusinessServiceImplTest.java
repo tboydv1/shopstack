9c7eec9bbc4420a512defeb1c91cfa4086d5c401
tboydv1 <tboydv1@gmail.com>
2020-02-12 09:07:27 +0100

    stash commit

---
+++ shopstack_service/src/test/java/com/shopstack/service/business/BusinessServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.shopstack.service.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author oluwatobi
+ *
+ */
+class BusinessServiceImplTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
